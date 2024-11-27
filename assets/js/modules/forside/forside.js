fecdc4e1d7eddcfbdadbeec0b1abf2cb385b3018
MikkelTMS <mtamsgaard@gmail.com>
2024-11-27 12:59:38 +0100

    Lidt fis og ballade
    Ja tak!

--- assets/js/modules/forside/forside.js
+++ assets/js/modules/forside/forside.js
@@ -1,8 +1,25 @@
 export const buildForside = () =>{
     const main = document.getElementById('main')
 
+    const forsideSection = document.createElement('section')
+    forsideSection.setAttribute('id', 'forsideSection')
+    main.append(forsideSection)
+
     const forsideHero = document.createElement('img');
     forsideHero.setAttribute('id', 'forsideHero')
     forsideHero.src = `/assets/img/Hero.png`
-    main.append(forsideHero)
+    forsideSection.append(forsideHero)
+
+    const forsideH2 = document.createElement('h2');
+    forsideH2.setAttribute('id', 'forsideH2')
+    forsideH2.innerText = `Fire tilfældige...`
+    forsideSection.append(forsideH2)
+
+    const forsideCardSection = document.createElement('forsideCardSection')
+    forsideCardSection = document.setAttribute('id', 'forsideCardSection')
+    forsideSection.append(forsideCardSection)
+
+    const forsideCard = document.createElement('forsideCard')
+    forsideCard = document.setAttribute('id', forsideCard)
+
 }
